e135713b9d9a66b855ea1c9137c9bb244a3678e9
Eli Stone <2546188+elistone@users.noreply.github.com>
2024-08-29 14:55:27 +0100

    feat: removing 404.html into dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -9,6 +10,14 @@ export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '404.html',
+          dest: './'
+        }
+      ]
+    })
   ],
   resolve: {
     alias: {
